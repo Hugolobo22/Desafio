5bfdd4409cf27fe3b19e7a09bcc49936c6213349
Hugolobo22 <150070939+Hugolobo22@users.noreply.github.com>
2024-03-14 10:03:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -41,12 +41,36 @@ saída =
 
 */
 
-let arrayPesos = [66.5, 40.0, 66.0];
-let arrayAlturas = [1.73, 1.58, 1.77];
-let arrayNomes = ['Hugo', 'Maria', 'Teles'];
+// Primeiramente, colocamos em base 15 personagens (fora as 3 primeiras pessoas que pegamos como base na aula de Terça,
+// todas as medidas foram feitas por pesquisas no google e algumas foram especuladas)
+
+console.log('Alunos: Hugo Medeiros e Pedro Felipe')
+
+let arrayPesos = [66.5, 40.0, 66.0, 79.3, 67.0, 50.9, 62.0, 56.0, 80.4, 37.0, 73.0, 140.0, 74.0, 79.0, 29.0];
+let arrayAlturas = [1.73, 1.58, 1.77, 1.80, 1.74, 1.66, 1.75, 1.65, 1.81, 0.90, 1.77, 1.69, 1.75, 1.73, 1.59];
+let arrayNomes = ['Hugo', 'Maria', 'Teles', 'Satoru gojou', 'Luffy', 'Naruto', 'Goku', 'Vegeta', 'Zoro', 'Chopper', 'Sanji', 'Thaís Carla', 'Megumi', 'Itadori', 'Bob esponja'];
 let arrayIMCs = [];
+let arrayDiag = []
+
+// Em seguida, fizemos a calculdora de IMC, utilizando o método conhecido e ensinado na sala, usando uma variável "i" como um contador
+// que passa de array em array para calcular seu imc
 
 for (let i = 0; i <= arrayPesos.length-1; i++){
 arrayIMCs[i] = arrayPesos[i] / (arrayAlturas[i] * arrayAlturas[i])
-console.log(arrayIMCs)
+	if (arrayIMCs[i] <= 18.5){ // Utilizando dos comandos if, else if e else, pegamos o valor do imc e verificamos o seu diagnóstico
+		arrayDiag[i] = 'Abaixo do peso' // Armazenamos o diagnóstico numa variável criada por nós, arrayDiag (utilizando ainda o índice "i" mas para armazenar o valor correto na variável correta)
+	}
+	else if(18.5 < arrayIMCs[i] && arrayIMCs[i] <= 25){ // Repetimos as ações acima para todos os diagnósticos
+		arrayDiag[i] = 'Peso normal'
+	}
+	else if(25 < arrayIMCs[i] && arrayIMCs[i] < 30){
+		arrayDiag[i] = 'Sobrepeso'
+	}
+	else if(30 <= arrayIMCs[i]){	
+	arrayDiag[i] = 'Obeso'
+	}
+
+	console.log(`Paciente (${i + 1}): Nome: ${arrayNomes[i]} 
+	- Peso: ${arrayPesos[i]} Kg - Altura: ${arrayAlturas[i]} m - IMC: ${arrayIMCs[i].toFixed(2)} - Diagnóstico: ${arrayDiag[i]}`)
 }
+// Ao final, utilizamos de template Strings para exibir todos os dados corretamente (usamos o toFixed para limitar o número do IMC em 2 dígitos após a vírgula)
